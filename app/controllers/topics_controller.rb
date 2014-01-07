63ef0c325ee945565f80205582ccd498b79093af
Rei <chloerei@gmail.com>
2014-01-07 14:14:32 +0800

    Set topics/posts order

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -2,7 +2,7 @@ class TopicsController < ApplicationController
   before_filter :require_logined, except: [:index, :show]
 
   def index
-    @topics = Topic.all
+    @topics = Topic.order(id: :desc)
   end
 
   def show
