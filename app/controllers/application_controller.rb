589e2e6cfb2c00b0449c4fb513c153102ca8abf2
Rei <chloerei@gmail.com>
2014-01-24 19:47:11 +0800

    Add httponly to remember_me token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -85,8 +85,9 @@ class ApplicationController < ActionController::Base
 
   def remember_me
     cookies[:remember_token] = {
-      :value   => current_user.remember_token,
-      :expires => 2.weeks.from_now
+      value: current_user.remember_token,
+      expires: 2.weeks.from_now,
+      httponly: true
     }
   end
 end
