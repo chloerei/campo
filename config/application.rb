9517d7cba209f34ca9852aae125ab0bbee30902e
Rei <chloerei@gmail.com>
2014-01-25 20:06:54 +0800

    Delete unuse helpers

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,10 @@ module Campo
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.generators do |g|
+      g.assets false
+      g.helper false
+    end
   end
 end
--- test/helpers/admin/application_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class Admin::ApplicationHelperTest < ActionView::TestCase
-end
