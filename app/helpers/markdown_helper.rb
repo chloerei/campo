1581ee933e1c15dcfb74ed594c5484da612bf8e3
Rei <chloerei@gmail.com>
2014-02-01 22:05:33 +0800

    Add campo_markdown helper

--- app/helpers/markdown_helper.rb
+++ app/helpers/markdown_helper.rb
@@ -5,7 +5,7 @@ module MarkdownHelper
     include Rouge::Plugins::Redcarpet
   end
 
-  def markdown(content)
+  def markdown(text)
     renderer = HTMLRender.new(hard_wrap: true,
                               filter_html: true,
                               link_attributes: { rel: 'nofollow' })
@@ -15,6 +15,50 @@ module MarkdownHelper
                                        space_after_headers: true,
                                        fenced_code_blocks: true)
 
-    markdown.render(content)
+    markdown.render(text)
+  end
+
+  def campo_markdown(text)
+    sanitize(link_comments(link_mentions(markdown(text))),
+             tags: %w(p br img h1 h2 h3 h4 blockquote pre code strong em a ul ol li span),
+             attributes: %w(href src class title alt target rel))
+  end
+
+  def link_mentions(text)
+    doc = Nokogiri::HTML.fragment(text)
+
+    doc.search('text()').each do |node|
+      unless node.ancestors('a, pre, code').any?
+        text = node.text
+
+        # link @username
+        text.gsub!(/@(\w+)/) { |match|
+          %Q|<a href="/~#{$1}">#{match}</a>|
+        }
+
+        node.replace text
+      end
+    end
+
+    doc.to_html
+  end
+
+  def link_comments(text)
+    doc = Nokogiri::HTML.fragment(text)
+
+    doc.search('text()').each do |node|
+      unless node.ancestors('a, pre, code').any?
+        text = node.text
+
+        # link #comment_id
+        text.gsub!(/#(\d+)/) { |match|
+          %Q|<a href="?comment_id=#{$1}">#{match}</a>|
+        }
+
+        node.replace text
+      end
+    end
+
+    doc.to_html
   end
 end
