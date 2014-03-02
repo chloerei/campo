dc55af79cd5c378e2142d868a80f3bf10a54107c
Rei <chloerei@gmail.com>
2014-03-02 21:21:13 +0800

    Remove unuse file

--- app/mailers/user_mailer_preview.rb
+++
@@ -1,7 +0,0 @@
-class UserMailerPreview < MailView
-  def password_reset
-    user = User.first || FactoryGirl.create(:user)
-    user.generate_password_reset_token
-    UserMailer.password_reset(user.id)
-  end
-end
