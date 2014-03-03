3587b31c950537aa086fc8691cafe56d0069c637
Rei <chloerei@gmail.com>
2014-03-03 22:28:20 +0800

    Use forwarded_port setting in Vagrantfile

--- config/deploy/vagrant.rb
+++ config/deploy/vagrant.rb
@@ -1,7 +1,8 @@
-server '192.168.33.10', {
+server '127.0.0.1', {
   user: 'vagrant',
   roles: %w{web app db},
   ssh_options: {
+    port: 2222,
     keys: %w(~/.vagrant.d/insecure_private_key)
   }
 }
