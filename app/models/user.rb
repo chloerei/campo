971ca332a0cf2f225575b6dbae5fd711c8d2c1cf
Rei <chloerei@gmail.com>
2014-03-18 00:56:34 +0800

    Fix username format validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
   has_many :likes, dependent: :delete_all
   has_many :attachments, dependent: :delete_all
 
-  validates :username, uniqueness: { case_sensitive: false }, presence: true, format: { with: /\A[a-z0-9-]+\z/i }
+  validates :username, uniqueness: { case_sensitive: false }, presence: true, format: { with: /\A[a-z0-9][a-z0-9-]*\z/i }
   validates :name, presence: true
   validates :email, uniqueness: { case_sensitive: false }, presence: true, format: { with: /\A([^@\s]+)@((?:[a-z0-9-]+\.)+[a-z]{2,})\z/i }
 
